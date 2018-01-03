cd0c55ff4cd92c28c5e8f7f7413c02ce450ae677
Al-assad <yulinying_1994@outlook.com>
2018-01-03 23:20:42 +0800

    update

--- src/site/assad/FlowerQJump/Configure.java
+++ src/site/assad/FlowerQJump/Configure.java
@@ -21,7 +21,7 @@ public class Configure {
 
 
     /** 按压时间和距离计算参数*/
-    public static double MAGIC_NUMBER = 1.36;
+    public static double MAGIC_NUMBER = 1.40;
 
     /** 显示图片缩放高度*/
     public  static int SCREEN_HEIGHT = 1000;
